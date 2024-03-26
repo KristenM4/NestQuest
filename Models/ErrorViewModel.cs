0ef966b2bef050dab2fb4a652c13c3acbb501bb7
Kristen <kristen4810@gmail.com>
2024-03-26 22:46:35 +0000

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NestQuest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
