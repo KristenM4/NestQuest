b3dd433292f6efdc03874d776ad723a907eab2c4
Kristen <kristen4810@gmail.com>
2024-03-28 21:27:41 +0000

    body and footer basic structure set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./**/*.{html,cshtml,razor,js}"],
-  theme: {
-    extend: {},
+    theme: {
+        container: {
+            center: true,
+            padding: '1rem'
+        },
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px'
+        },
+        extend: {},
   },
   plugins: [],
 }
