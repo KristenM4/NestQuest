32c5347caa245039544b648a74d62e426c86405c
Kristen <kristen4810@gmail.com>
2024-04-01 21:22:01 +0100

    create property model class

---
+++ Models/Property.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NestQuest.Models;
+public class Property
+{
+    public int Id { get; set; }
+    public string? StreetNumber { get; set; }
+    public string? StreetName { get; set; }
+    public string? City { get; set; }
+    public string? PostCode { get; set; }
+    public string? Description { get; set; }
+
+    [Column(TypeName = "decimal(15, 2)")]
+    public decimal Price { get; set; }
+    public int Bedrooms { get; set; }
+    public int Bathrooms { get; set; }
+}
